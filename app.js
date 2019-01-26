cd17a4328a913d6d9d6fca9c767795c2afff6b25
NicKAnderson038 <andersonnick038@gmail.com>
2019-01-25 17:45:11 -0800

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require('./routes/feed')
 
-const app = express();
+const app = express()
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-app.use(bodyParser.json()); // application/json
+app.use(bodyParser.json()) // application/json
 
-app.use('/feed', feedRoutes);
+// Register routes
+app.use('/feed', feedRoutes)
 
-app.listen(8080);
+app.listen(8080)
--- routes/feed.js
+++ routes/feed.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require('express')
 
-const feedController = require('../controllers/feed');
+const feedController = require('../controllers/feed')
 
-const router = express.Router();
+const router = express.Router()
+
+// ***** BEGIN: api routes *****
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', feedController.getPosts)
 
 // POST /feed/post
-router.post('/post', feedController.createPost);
+router.post('/post', feedController.createPost)
 
-module.exports = router;
+// ***** END: api routes *****
+
+module.exports = router
