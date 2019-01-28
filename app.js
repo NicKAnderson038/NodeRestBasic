7cd1e3d468535ee08332cd57ee76e84058036959
NicKAnderson038 <andersonnick038@gmail.com>
2019-01-27 21:09:00 -0800

    cors support added

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express()
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()) // application/json
 
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
+
 // Register routes
 app.use('/feed', feedRoutes)
 
